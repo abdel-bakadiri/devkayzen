a613e90fb6d378a18792233e743ffd9e857f0cdf
abdel-bakadiri <abdelouahab.bakadiri@gmail.com>
2020-10-04 16:32:07 +0200

    Feat(): convert to number

---
+++ 02-10-2020/mdb/convert-to-number/main.js
@@ -0,0 +1,17 @@
+let a = "12";
+a = Number(a);
+console.log(a);
+console.log(typeof a);
+
+let b = "12";
+b = +b;
+console.log(b);
+console.log(typeof b);
+
+let c = "12";
+c = parseInt(c);
+console.log(c);
+console.log(typeof c);
+
+// we can do the same for a boolean value
+
