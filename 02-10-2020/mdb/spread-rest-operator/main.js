1b23fa7d8029ed0a8a7f718d7cccff80c7034d2f
abdel-bakadiri <abdelouahab.bakadiri@gmail.com>
2020-10-05 23:13:29 +0200

    Feat(): spread and rest operators

---
+++ 02-10-2020/mdb/spread-rest-operator/main.js
@@ -0,0 +1,19 @@
+const nestedArray = [3, 4];
+const array = [1, 2, ...nestedArray, 5, 6];
+console.log(array); // [ 1, 2, 3, 4, 5, 6]
+
+let obj1 = {
+  one: "one1",
+};
+let obj = {
+  ...obj1,
+  two: "two",
+};
+console.log(obj);
+
+function sum(...arg) {
+  return arg.reduce((current, next) => current + next);
+}
+console.log(sum(1, 1)); //
+console.log(sum(2, 1, 1, 1)); // 5
+console.log(sum(2, 2, 0,2,3,5)); // 14
